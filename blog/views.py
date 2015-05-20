48784d28993fbf748ad14c4c36424733314897c7
Paivi <peconia@gmail.com>
2015-05-20 21:12:13 +0100

    cleaning up

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,13 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.template import Context, loader
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+
+    return render(request, 'blog/post_list.html', {'posts': posts })
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -57,8 +60,4 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('blog.views.post_list')
-    
-
-
-
     
